5058e33e6631352b631ba74bbd1b1f9b9f7eace7
open-meadow <rohan.varughese@protonmail.com>
2023-01-25 23:51:58 +0000

    Add new categories and alert when category has been created

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -4,4 +4,26 @@ class Admin::CategoriesController < ApplicationController
     @categories = Category.all
   end
 
+  def new
+    @categories = Category.new
+  end
+
+  def create
+    @categories = Category.new(category_params)
+
+    if @categories.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(
+      :name,
+    )
+  end
+
 end
