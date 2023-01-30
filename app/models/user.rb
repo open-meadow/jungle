04f2b3b6811f480e89a698f0e0e169886580aff2
open-meadow <rohan.varughese@protonmail.com>
2023-01-30 21:41:14 +0000

    Initial tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ApplicationRecord
 
   has_secure_password
 
+  validates :name, presence: true
+  validates :password, presence: true
+  validates :password_confirmation, presence: true
+  validates :email, presence: true
+
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,63 @@
+require 'rails_helper'
+
+# RSpec.describe User, type: :model do
+#   pending "add some examples to (or delete) #{__FILE__}"
+# end
+
+RSpec.describe User, type: :model do
+  describe 'User Validations' do
+
+    it "The user has everything" do
+      @users = User.new(name: "Mister Popo", password: "popo", password_confirmation:"popo", email: "mister@popo.com")
+      
+      @users.save
+
+      expect(@users).to be_present
+    end
+
+    it "throw error if name is not present" do
+      @users = User.new(name: nil, password: "popo", password_confirmation:"popo", email: "mister@popo.com")
+      
+      @users.save
+      expect(@users).to_not be_valid
+      # expect(@users.errors.full_messages).to include("Name can't be blank")
+
+    end
+
+    it "throw error if password is not present" do
+      @users = User.new(name: "Mister popo", password: nil, password_confirmation:"popo", email: "mister@popo.com")
+      
+      @users.save
+
+      expect(@users.errors.full_messages).to include("Password can't be blank")
+
+    end
+
+    it "throw error if password_confirmation is not present" do
+      @users = User.new(name: "Mister popo", password: "popo", password_confirmation: nil, email: "mister@popo.com")
+      
+      @users.save
+
+      expect(@users).to_not be_valid
+    end
+
+    it "throw error if password_confirmation is not the same as password" do
+      @users = User.new(name: "Mister popo", password: "popo", password_confirmation: "opop", email: "mister@popo.com")
+      
+      @users.save
+
+      expect(@users.errors.full_messages).to include("Password confirmation doesn't match Password")
+
+    end
+
+    it "throw error if email is not present" do
+      @users = User.new(name: "Mister popo", password: "popo", password_confirmation: "popo", email: nil)
+      
+      @users.save
+
+      expect(@users.errors.full_messages).to include("Email can't be blank")
+
+    end
+
+  end
+end
