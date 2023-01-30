4b6b9b2c3c72b022c011b7820b0f4b06d5ab756e
open-meadow <rohan.varughese@protonmail.com>
2023-01-30 22:05:57 +0000

    Add checks for e-mail and password

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :name, presence: true
-  validates :password, presence: true
+  validates :password, presence: true, length: { minimum: 4 }
   validates :password_confirmation, presence: true
-  validates :email, presence: true
+  validates :email, presence: true,  uniqueness: { case_sensitive: false }
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,13 +12,14 @@ RSpec.describe User, type: :model do
       
       @users.save
 
-      expect(@users).to be_present
+      expect(@users).to be_valid
     end
 
     it "throw error if name is not present" do
       @users = User.new(name: nil, password: "popo", password_confirmation:"popo", email: "mister@popo.com")
       
       @users.save
+
       expect(@users).to_not be_valid
       # expect(@users.errors.full_messages).to include("Name can't be blank")
 
@@ -56,6 +57,29 @@ RSpec.describe User, type: :model do
       @users.save
 
       expect(@users.errors.full_messages).to include("Email can't be blank")
+
+    end
+
+    it "throw error if email exists in the database" do
+      @users = User.new(name: "Mister popo", password: "popo", password_confirmation: "popo", email: "tristanjacobs@gmail.com")
+
+      @users2 = User.new(name: "Mister popo", password: "popo", password_confirmation: "popo", email: "TristanJacobs@gmail.com")
+      
+      @users.save
+      @users2.save
+
+      expect(@users2).to_not be_valid
+      # expect(@users.errors.full_messages).to include("Email alre")
+
+    end
+
+    it "password should have minimum length" do
+      @users = User.new(name: "Mister popo", password: "pop", password_confirmation: "pop", email: "tristanjacobs@gmail.com")
+      
+      @users.save
+
+      expect(@users).to_not be_valid
+      # expect(@users.errors.full_messages).to include("Email alre")
 
     end
 
