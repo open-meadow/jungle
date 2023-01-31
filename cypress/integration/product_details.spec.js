fcbff2744446bcf0c6c33862e554485b6df6dfc7
open-meadow <rohan.varughese@protonmail.com>
2023-01-31 12:46:53 -0500

    Add correctly passing tests

--- cypress/integration/product_details.spec.js
+++ cypress/integration/product_details.spec.js
@@ -4,6 +4,14 @@ describe('Jungle Product Details Page', () => {
     cy.visit("/");
   });
 
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+
+  it("There is 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  });
+  
   // test that users can navigate from the home page to the product details page
   it("Shows product/show", () => {
     cy.get(".products article").first().click();
