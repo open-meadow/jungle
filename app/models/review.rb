b748950297d22d1de71f14a265cfb9f5df49f7bb
open-meadow <rohan.varughese@protonmail.com>
2023-01-31 22:50:08 +0000

    Confirm the ability to add new reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,8 +2,8 @@ class Review < ApplicationRecord
 
   belongs_to :product
 
-  validates :product_id, presence: true,
-  validates :user_id, presence: true,
-  validates :rating, presence: true,
+  validates :product_id, presence: true
+  validates :user_id, presence: true
+  validates :rating, presence: true
 
 end
