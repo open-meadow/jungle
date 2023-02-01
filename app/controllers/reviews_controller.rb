734f38e68a1e29cf0fbf2e5934e66671a985746e
open-meadow <rohan.varughese@protonmail.com>
2023-02-01 15:00:51 -0500

    Add delete button

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,14 +1,6 @@
 class ReviewsController < ApplicationController
-  def create
-    # @review = params[:review]
-    # @review[:product_id] = params[:product_id]
-    # @review[:user_id] = session[:user_id]
 
-    # @review = Review.create()
-    
-    
-    # @review.user = current_user
-    
+  def create    
     req_params = review_params
     req_params[:user_id] = session[:user_id]
     @review = Review.new(req_params)
@@ -20,7 +12,12 @@ class ReviewsController < ApplicationController
       redirect_to "/products/#{req_params[:product_id]}"
     end
     
-    
+  end
+
+  def destroy
+    @review = Review.find params[:id]
+    @review.destroy
+    redirect_to "/products/#{@review[:product_id]}"
   end
 
   private
